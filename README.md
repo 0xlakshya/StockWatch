796bcb8198d09ccb537002110bbb2054323c9f8e
lakshya-dhariwal <lakshya@staq.com>
2023-09-24 23:27:55 +0530

    add historical graph

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # StockWatch
+
+### A demo is worth a 1000 words
+
+Tech Stack : TS , NextJS, NodeJS , Express , TailwindCSS, Apache eCharts
+
+
+## Setup
+
+```
+git clone
+cd server
+
+```
+
+add env for server
+PORT=8000
+JWT_SECRET=<"secret token for jwt">
+
+then
+
+```
+yarn && yarn start
+```
+
+```
+cd client
+```
+
+add env for client
+NEXT_PUBLIC_BACKEND='<backend_url>'
+
+then
+
+```
+yarn && yarn start
+
+```
--- client/src/components/Dashboard/HistoricalGraph.tsx
+++ client/src/components/Dashboard/HistoricalGraph.tsx
@@ -1,5 +1,34 @@
+import ReactECharts from "echarts-for-react";
+
 function HistoricalGraph(props: { data?: IHistorical[] | null }) {
-  return <div>hi</div>;
+  const dates = props.data?.map((item) => item.date);
+  const prices = props.data?.map((item) => item.price);
+  const option = {
+    xAxis: {
+      type: "Dates",
+      boundaryGap: false,
+      data: dates,
+    },
+    yAxis: {
+      type: "Prices",
+    },
+    series: [
+      {
+        data: prices,
+        type: "line",
+        areaStyle: {},
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <h1 className=" text-2xl font-semibold leading-none tracking-tight my-2">
+        NIFTY 50
+      </h1>
+      <ReactECharts option={option} />
+    </div>
+  );
 }
 
 export default HistoricalGraph;
